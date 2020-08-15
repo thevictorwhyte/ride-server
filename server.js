5217d7a19698ac7d8d63197b821d7d373290f46e
thevictorwhyte <victordavidwhyte@gmail.com>
2020-08-15 17:40:57 +0100

    deployed to heroku

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 
 //views
